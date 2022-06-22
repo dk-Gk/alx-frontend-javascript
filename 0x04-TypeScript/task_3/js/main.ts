2588bbddbc6c613443f234a033ba6251df523fb7
dk-Gk <idkin19@gmail.com>
2022-06-22 18:54:53 +0000

    typescript

---
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -0,0 +1,15 @@
+import { RowID, RowElement } from './interface.ts';
+import * as CRUD from './crud.js';
+
+const row: RowElement {
+  firstName: Guillaume,
+  lastName: Salva.
+};
+
+const newRowID: RowID = CRUD.insertRow(row);
+
+const updatedRow: RowElement = { firstName: "Guillaume", lastName: "Salva", age: 23 };
+
+CRUD.updateRow(newRowID, updatedRow);
+
+CRUD.deleteRow(newRowID);
